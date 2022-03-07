89e480b69187d3c4b34ef86c695a3f7225477189
John West <contactjohnwest@gmail.com>
2022-03-07 05:03:04 -0800

    Add playerSelection and playRound functions

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-function computerPlay() {
+function computerSelection() {
     let randomInt = Math.floor((Math.random() * 3) + 1);
     switch (randomInt) {
         case 1:
-            return 'Rock';
+            return "Rock";
         case 2:
-            return 'Paper';
+            return "Paper";
         case 3:
-            return 'Scissors';    
+            return "Scissors";    
     }
 }
 
+function playerSelection() {
+    let playerChoice = window.prompt("Choose Rock, Paper, or Scissors: ");
+    switch (playerChoice.toLowerCase()) {
+        case "rock":
+            return "Rock";
+        case "paper":
+            return "Paper";
+        case "scissors":
+            return "Scissors";
+        default:
+            return "invalid choice";    
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection)
+        return "Tie you both chose " + playerSelection;
+    else if (playerSelection === "Rock" && computerSelection === "Scissors")
+        return "You Win! Rock beats Scissors";
+    else if (playerSelection === "Rock" && computerSelection === "Paper")
+        return "You Lose! Paper beats Rock";
+    else if (playerSelection === "Paper" && computerSelection === "Rock")
+        return "You Win! Paper beats Rock";
+    else if (playerSelection === "Paper" && computerSelection === "Scissors")
+        return "You Lose! Scissors beats Paper";
+    else if (playerSelection === "Scissors" && computerSelection === "Paper")
+        return "You Win! Scissors beats Paper";
+    else if (playerSelection === "Scissors" && computerSelection === "Rock")
+        return "You Lose! Rock beats Scissors";
+}
+
+const player = "Rock";
+const computer = computerSelection();
+console.log(playRound(player, computer));
