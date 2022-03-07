c37229faddd3512340e98518541f4c66adf94e1f
John West <contactjohnwest@gmail.com>
2022-03-07 05:25:36 -0800

    Add game function, changed playRound to return a number

--- script.js
+++ script.js
@@ -25,22 +25,57 @@ function playerSelection() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection)
-        return "Tie you both chose " + playerSelection;
-    else if (playerSelection === "Rock" && computerSelection === "Scissors")
-        return "You Win! Rock beats Scissors";
-    else if (playerSelection === "Rock" && computerSelection === "Paper")
-        return "You Lose! Paper beats Rock";
-    else if (playerSelection === "Paper" && computerSelection === "Rock")
-        return "You Win! Paper beats Rock";
-    else if (playerSelection === "Paper" && computerSelection === "Scissors")
-        return "You Lose! Scissors beats Paper";
-    else if (playerSelection === "Scissors" && computerSelection === "Paper")
-        return "You Win! Scissors beats Paper";
-    else if (playerSelection === "Scissors" && computerSelection === "Rock")
-        return "You Lose! Rock beats Scissors";
+    if (playerSelection === computerSelection) {
+        console.log("Tie you both chose " + playerSelection);
+        return 0;
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("You Win! Rock beats Scissors");
+        return 1;
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("You Lose! Paper beats Rock");
+        return 2;
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("You Win! Paper beats Rock");
+        return 1;
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("You Lose! Scissors beats Paper");
+        return 2;
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log("You Win! Scissors beats Paper");
+        return 1;
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("You Lose! Rock beats Scissors");
+        return 2;
+    }
 }
 
-const player = "Rock";
-const computer = computerSelection();
-console.log(playRound(player, computer));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let winner = 0;
+    for (let i = 0; i < 5; i++) {
+        winner = playRound(playerSelection(), computerSelection());
+        switch (winner) {
+            case 1:
+                playerScore++;
+                break;
+            case 2:
+                computerScore++;
+                break;
+            default:
+                break;
+        }
+    }
+    if (playerScore > computerScore)
+        console.log("You won with a score of " + playerScore);
+    else if (playerScore < computerScore)
+        console.log("You lose, the computer scored " + computerScore);
+    else
+        console.log("You tied with a score of " + playerScore);
+}
